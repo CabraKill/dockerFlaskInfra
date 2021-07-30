73962178be65340dc940049bec753593b77fb7e9
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-07-30 09:40:25 -0300

    Fix endpoints from EndpointController

--- src/endpointController.py
+++ src/endpointController.py
@@ -1,3 +1,4 @@
+from flask import request
 from src import app
 
 
@@ -8,5 +9,31 @@ class EndpointController():
     def test(self):
         return "Testing subroute... OK!"
 
+    def test_body(self):
+        return f"Testing body received: {request.data}"
+    
+    def test_body_json(self):
+        """
+        Expects a Content-Type json header 
+        """
+        return f"Testing body received: {request.json}"
+
+    @app.route('/testWithAnnotation')
+    def testWithAnnotation():
+        return "Testing annotation... OK!"
+
+    def testWithArgs(name):
+        name = request.args.get('name')
+        return f'Name: {name}'
+
+    def testWithData(self, title):
+        return f'Data: {title}'
+
     def setEndpoints(self) -> None:
         app.add_url_rule('/test', view_func=self.test, methods=['GET'])
+        app.add_url_rule('/test', view_func=self.test_body, methods=['POST'])
+        app.add_url_rule('/test-json', view_func=self.test_body_json, methods=['POST'])
+        app.add_url_rule(
+            '/test-args', view_func=self.testWithArgs, methods=['GET'])
+        app.add_url_rule('/test/<string:title>',
+                         view_func=self.testWithData, methods=['GET'])
